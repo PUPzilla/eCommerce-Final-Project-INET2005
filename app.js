27b53facb7cc6d754f73794d77ba710955d0c373
Gavin <gavinmacdonald2000@hotmail.com>
2024-11-17 14:34:03 -0400

    started router stuff

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//test2
+import express from 'express';
+import UserRouter from './routes/users.js';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Express test');
+});
+
+app.listen(port, () => {
+    console.log(`app.js from YotasTacos listening on port: ${port}`);
+});
--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,25 @@
-//test2
+import express from 'express';
+
+const UserRouter = express.Router();
+
+router.get('/users/all', (req, res) => {
+    res.send('All Users Route');
+});
+
+router.post('/users/signup', (req, res) => {
+    res.send(`SignUp route`);
+});
+
+router.post('users/login', (req, res) => {
+    res.send(`Login Route`);
+});
+
+router.post(`/users/logout`, (req, res) => {
+    res.send(`Logout Route`);
+});
+
+router.get('/users/getsession', (req, res) => {
+    res.send(`Get User Session`);
+});
+
+export default UserRouter;
