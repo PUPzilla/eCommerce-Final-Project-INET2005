72ef059e61ae507f1571565471d32bc9982663ea
Gavin <gavinmacdonald2000@hotmail.com>
2024-12-03 15:28:39 -0400

    Redoing cart

--- TacoReact/src/routes/Cart.jsx
+++ TacoReact/src/routes/Cart.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import Card from '../ui/card';
+import { useState, useEffect } from "react";
 
 export default function Cart() {
+    
+    const [cartProdID, setProdID] = useState(null);
+
     return(
         <>
         <h1>Cart Page</h1>
